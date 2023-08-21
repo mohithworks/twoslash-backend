ae26795eb1a0e842ac427dfc1df22fa79f566578
mohithworks <mohithworks2002@gmail.com>
2023-08-21 16:46:34 +0530

    Added auto renewal & other features

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post("/getApi", async function (req, res, next) {
           
           const match = dateDif.match(/\d+d/);
           const noofDays = parseInt(match ? match[0].replace("d", "") : 0);
-          const noofDaysOver = noofDays > 31 ? 31 : noofDays;
+          const noofDaysOver = noofDays > 30 ? 30 : noofDays;
 
           if(trial_days_over !== noofDaysOver) { 
             var subVal = noofDaysOver - trial_days_over;
@@ -304,9 +304,176 @@ app.post("/getPaymentStart", async function (req, res, next) {
 
 });
 
+app.get("/getInvoice", async function (req, res, next) { 
+  const invoice = await stripe.invoices.retrieve(
+    req.query.invoice
+  );
+  var invoiceMilli = invoice.created * 1000;
+  var invoiceMilliD = "2027-04-05T00:14:59.000Z";
+  var invoiceDate = new Date(invoiceMilli)
+  var invoiceDate2 = new Date(invoiceMilliD)
+
+  console.log(invoiceDate);
+  console.log(invoiceDate2);
+  var dateDif = dateDifference(invoiceDate, invoiceDate2);
+
+  const match = dateDif.match(/\d+d/);
+  const noofDays = parseInt(match ? match[0].replace("d", "") : 0);
+  // Calculate the difference in millisecond
+  const datenw = new Date();
+  console.log(dateDif);
+  console.log(datenw);
+  console.log(noofDays);
+  res.send(invoice);
+})
+
+app.get("/getSub", async function (req, res, next) { 
+  const subscription = await stripe.subscriptions.list({
+    customer: req.query.customer,
+    status: 'active',
+  });
+  if(subscription && subscription["data"].length === 0) { 
+    res.status(404).send({
+      status: 404,
+      data: "No Active Subcriptions"
+    });
+    return;
+  }
+  const autoRenew = subscription["data"][0].collection_method === "send_invoice" ? false : true;
+  console.log(autoRenew)
+  res.status(200).send({
+    status: 200,
+    data: autoRenew
+  });
+});
+
+app.get("/getSub2", async function (req, res, next) { 
+  const subscription = await stripe.subscriptions.list({
+    customer: req.query.customer,
+  });
+  res.send(subscription)
+});
+
+app.get("/updateSub", async function (req, res, next) { 
+  const subscription = await stripe.subscriptions.update(
+    req.query.subId,
+    {
+      billing_cycle_anchor: 'now', proration_behavior: 'none',
+    }
+  );
+  var deleted = await stripe.invoices.finalizeInvoice(
+    subscription.latest_invoice,
+  );
+  if(deleted.status === "open") {
+    console.log("In void")
+     deleted = await stripe.invoices.voidInvoice(
+      subscription.latest_invoice,
+    );
+  }
+  res.send(deleted)
+});
+
+app.get("/deleteInvoice", async function (req, res, next) { 
+  var deleted = stripe.invoices.finalizeInvoice(
+    req.query.invoice,
+  );
+  if(deleted.status === "open") {
+     deleted = await stripe.invoices.voidInvoice(
+      req.query.invoice,
+    );
+  }
+  res.send(deleted)
+});
+
+app.get("/updateSubStat", async function (req, res, next) { 
+  const subscription = await stripe.subscriptions.list({
+    customer: req.query.customer,
+  });
+
+  if(subscription && subscription["data"].length > 1) { 
+    res.status(400).send({
+      status: 400,
+      message: "Subscription not found"
+    });
+    return;
+  }
+  const subId = subscription["data"][0].id;
+  const collectionType = req.query.type === "enable" ? "charge_automatically" : "send_invoice";
+  const dayDue = req.query.type === "disable" ? { days_until_due: 30 } : {};
+
+  await stripe.subscriptions.update(
+    subId,
+    {collection_method: collectionType, ...dayDue}
+  )
+  res.status(200).send({
+    status: 200,
+    message: "OK"
+  });
+});
+
+app.get("/finInv", async function (req, res, next) { 
+  var deleted = await stripe.invoices.finalizeInvoice(
+    req.query.invoice,
+  );
+  res.send(deleted)
+});
+
+app.get("/createeditPayMode", async function (req, res, next) { 
+  const customerId = req.query.customer;
+
+  const subscription = await stripe.subscriptions.list({
+    customer: customerId,
+  });
+
+  console.log(subscription["data"][0].id);
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: 'https://twoslash.ai/payment-successful/',
+    mode: 'setup',
+    customer: customerId,
+    setup_intent_data: {
+      metadata: {
+        subscription_id: subscription["data"][0].id,
+      },
+    },
+  });
+
+  var response;
+  var origin = 'POST';
+  try{
+      origin = req.headers.origin;
+  }catch(e){
+    response = {
+          statusCode: 400,
+          body: {"status":400, "data": "error"},
+    };
+  }
+
+  if(origin.includes('chrome-extension://') || origin == 'https://checkout.stripe.com'){
+    if(session) {
+      const checkoutUrl = session.url;
+      response = {
+            statusCode: 200,
+            body: {"status":200, "data": checkoutUrl},
+      };
+    }else {
+      response = {
+            statusCode: 400,
+            body: {"status":400, "data": "error"},
+      };
+    }
+  }else {
+  
+    response = {
+      statusCode: 400,
+      body: {"status": 400, "data": "error"},
+    };
+  }
+  res.status(response.statusCode).send(response)
+});
+
 app.post("/createStripePayment", async function (req, res, next) {
-  console.log(req.headers);
-  console.log(req.query);
   const userid = req.query.userid;
   const email = req.query.email;
   const phoneno = req.query.phone;
@@ -325,12 +492,13 @@ app.post("/createStripePayment", async function (req, res, next) {
     .select('stripe_customer_id')
     .eq('id', userid)
     
-    if(error) {
-      console.log(error);
+    if(error || data.length === 0) {
       response = {
           statusCode: 400,
-          body: {"status":"400", "origin":origin, "error":error},
+          body: {"status":"400", "origin":origin, "error":"There was an error"},
       }; 
+      res.status(response.statusCode).send(response);
+      return;
     }
 
     if(data[0].stripe_customer_id === null) {
@@ -343,21 +511,45 @@ app.post("/createStripePayment", async function (req, res, next) {
       });
       customer = stripeCustomer.id;
     } else {
-      console.log(data[0].stripe_customer_id)
       customer = data[0].stripe_customer_id;
     }
 
     if(accountType === 'Subscription') { 
 
-      session = await stripe.checkout.sessions.create({
-        success_url: 'https://twoslash.ai/payment-successful/',
-        line_items: [
-          {price: process.env.STRIPE_SUBSCRIPTION_PRICE_ID, quantity: 1},
-        ],
-        mode: 'payment',
-        customer: customer,
-        allow_promotion_codes: true,
+      const subscription = await stripe.subscriptions.list({
+        customer,
       });
+
+      if(subscription && subscription["data"].length === 0) { 
+
+        session = await stripe.checkout.sessions.create({
+          payment_method_types: ['card'],
+          success_url: 'https://twoslash.ai/payment-successful/',
+          line_items: [
+            {price: process.env.STRIPE_SUBSCRIPTION_PRICE_ID, quantity: 1},
+          ],
+          mode: 'subscription',
+          customer: customer,
+          allow_promotion_codes: true,
+        });
+
+      }else {
+        const invoiceId = subscription["data"][0].latest_invoice;
+        const invoice = await stripe.invoices.retrieve(invoiceId);
+        const invoiceStat = invoice.status;
+        if(invoiceStat === "paid" || invoiceStat === "draft") {
+          response = {
+              statusCode: 404,
+              body: {"status":"404", "origin":origin, "error":"No Open Invoices"},
+          }; 
+          res.status(response.statusCode).send(response);
+          return;
+        }
+        session = {
+          id: invoiceId,
+          url: invoice.hosted_invoice_url,
+        }
+      }
 
     } else {
 
@@ -402,7 +594,7 @@ app.post("/createStripePayment", async function (req, res, next) {
       };
     }
       
-    res.send(response);
+    res.status(response.statusCode).send(response);
   
   //}else {
   // res.send("Invalid Request");
@@ -424,106 +616,161 @@ app.post('/stripePayVerify', bodyParser.raw({type: '*/*'}), async (req, res) => 
     event = stripe.webhooks.constructEvent(body, sig, secret);
   }
   catch (err) {
-    res.status(400).send(`Webhook Error: ${err.message}`);
+    res.status(200).send(`Webhook Error: ${err.message}`);
   }
   console.log(event);
-	if(event.type === 'checkout.session.completed') {
+	if(event.type === 'charge.succeeded') {
       console.log(event.data.object);
       const paymentIntent = event.data.object;
       console.log('PaymentIntent was successful!');
 
       const order_id = paymentIntent.id;
       const customerID = paymentIntent.customer;
-      const amount = paymentIntent.amount_total;
+      const amount = paymentIntent.amount;
+
+      const invoiceId = paymentIntent.invoice;
+      var noofDays;
+
+      if(invoiceId !== null) {
+        const invoice = await stripe.invoices.retrieve(invoiceId);
+
+        var invoiceDate = new Date(invoice.created * 1000);
+        var currentDate = new Date();
+
+        console.log(invoiceDate);
+        console.log(currentDate);
+        var dateDif = dateDifference(invoiceDate, currentDate);
+
+        const match = dateDif.match(/\d+d/);
+        noofDays = parseInt(match ? match[0].replace("d", "") : 0);
+        console.log(noofDays);
+      }
+
+      if(noofDays > 0) {
+        const cusSubscription = await stripe.subscriptions.list({
+          customer: customerID,
+        });
+        const cusSubscriptionId = cusSubscription["data"][0].id;
+        const subCollMethod = cusSubscription["data"][0].collection_method;
+        const subCollMethodU = subCollMethod === "charge_automatically" ? true : false;
+
+        const subscription = await stripe.subscriptions.update(
+          cusSubscriptionId,
+          {
+            billing_cycle_anchor: 'now', proration_behavior: 'none',
+            collection_method: "send_invoice", days_until_due: 30,
+          }
+        );
+        
+        var deleted = await stripe.invoices.finalizeInvoice(subscription.latest_invoice);
+        if(deleted.status === "open") {
+          console.log("In void")
+          await stripe.invoices.voidInvoice(subscription.latest_invoice);
+        }
+
+        if(subCollMethodU) {
+          await stripe.subscriptions.update(
+            cusSubscriptionId,
+            {
+              collection_method: "charge_automatically"
+            }
+          );
+        }
+        console.log("deleted")
+      }
 
       const customer = await stripe.customers.retrieve(customerID);
-      const price = await stripe.checkout.sessions.listLineItems(
-        order_id,
-        { limit: 1 },
-      );
-      const priceId = price.data[0].price.id;
 
-      const { email, phone, metadata } = customer;
+      const { metadata } = customer;
     
       const userid = metadata.userid;
 
       console.log(customer);
       console.log('request is legit')
   
-      var { date, totalDays } = getDate();
+      var { date } = getDate();
   
-      console.log(date, totalDays)
-  
-      const transStartData = await supabase
+      console.log(date)
+
+      const stripe_sub_amount = process.env.STRIPE_SUBSCRIPTION_AMOUNT * 100;
+
+      var trial = amount == stripe_sub_amount ? 30 : 0;
+      var accountType = amount == stripe_sub_amount  ? "Subscription" : "Lifetime";
+      var prompts = amount == stripe_sub_amount ? 1000 : 20;
+
+      var updateData = { 
+        prompts: prompts, 
+        orderid: order_id, 
+        status: "Completed",
+        order_date: date,
+        trial: trial,
+        account_type: accountType,
+        trial_days_over: 0,
+      }
+
+      const { data, error } = await supabase
       .from('users')
-      .select("*")
-      .eq('transEmail', email)
-      .eq('transPhone', phone)
+      .update(updateData)
       .eq('id', userid)
-  
-      if(transStartData.error) { 
-        console.log(transStartData.error)
+
+      if(error) { 
+        res.status(200).json({ status: 'Error' })
       }
-      if(transStartData.data.length > 0) { 
-  
-        console.log('Yes 1')
-  
-        var trial = priceId == process.env.STRIPE_SUBSCRIPTION_PRICE_ID ? totalDays : 0;
-        var accountType = priceId == process.env.STRIPE_SUBSCRIPTION_PRICE_ID  ? "Subscription" : "Lifetime";
-        var prompts = priceId == process.env.STRIPE_SUBSCRIPTION_PRICE_ID ? 1000 : 0;
-  
-        console.log('Yes')
-  
-        var updateData = { 
-          prompts: prompts, 
-          orderid: order_id, 
-          status: "Completed",
-          order_date: date,
-          trial: trial,
-          account_type: accountType,
-          trial_days_over: 0,
-        }
-  
-        const { data, error } = await supabase
-        .from('users')
-        .update(updateData)
-        .eq('transEmail', email)
-        .eq('transPhone', phone)
-        .eq('id', transStartData.data[0].id)
-  
-        if(error) { 
-          console.log(error)
-        }
-        if(data) {
-          console.log('Yes 2')
-          res.json({ status: 'ok' })
-        }
-  
-      }else {
-        const insertData = [{
-          transEmail: email,
-          transPhone: phone,
-          payid: customerID,
-          orderid: order_id,
-        }]
-        const { data } = await supabase
-          .from('transactions')
-          .insert(insertData).select()
-  
-        // if(error) { 
-        //   res.json({ status: 'Error' })
-        // }
-        if(data) {
-          res.json({ status: 'ok' })
-        }
-  
+      if(data) {
+        res.status(200).json({ status: 'ok' })
       }
+  
   }else {
   
-	  res.json({ status: 'Request invalid' })
+	  res.status(200).json({ status: 'Request invalid' })
 
 	}
-})
+});
+
+const editWebhookSecret = process.env.STRIPE_EDIT_WEBHOOK_SECRET;
+
+app.post('/editPayMode', bodyParser.raw({type: '*/*'}), async (req, res) => {
+	// do a validation
+  const sig = req.headers['stripe-signature'];
+  const body = req.body.toString()
+
+  console.log(req.headers);
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(body, sig, editWebhookSecret);
+  }
+  catch (err) {
+    res.status(200).send(`Webhook Error: ${err.message}`);
+  }
+	if(event.type === 'checkout.session.completed') { 
+
+    const session = event.data.object;
+    if(session.mode === "setup") {
+      
+      const setupIntentId = session.setup_intent;
+
+      const setupIntent = await stripe.setupIntents.retrieve(setupIntentId);
+
+      console.log(setupIntent);
+      const paymentMode = setupIntent.payment_method;
+      const subId = setupIntent.metadata.subscription_id;
+
+      await stripe.subscriptions.update(subId, {
+        default_payment_method: paymentMode,
+      });
+
+      res.status(200).send({ message: "OK" });
+    }else {
+      res.status(200).json({ status: 'Error' });
+    }
+  }else {
+	  res.status(200).json({ status: 'Request invalid' })
+	}
+});
+
+
+app.use(bodyParser.json())
 
 app.post('/check', async (req, res) => { 
   const order_id = req.query.order_id;
@@ -535,8 +782,6 @@ app.post('/check', async (req, res) => {
   );
   res.send(price.data[0].price.id);
 });
-
-app.use(bodyParser.json())
 
 app.listen(port, () => console.log(`server started on port ${port}`));
 
